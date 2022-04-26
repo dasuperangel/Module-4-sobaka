e67410e3bd27f9747ee1d8b0c3120362521282d6
dasuperangel <dasuperangel@gmail.com>
2022-04-26 18:26:13 +0300

    Добавьте файлы проекта.

---
+++ Module 4 sobaka/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4_sobaka
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			(string Name, string Type, double Age, int NameCount) Pet;
+
+			Console.WriteLine("Введите имя питомца");
+			Pet.Name = Console.ReadLine();
+			Pet.NameCount = Pet.Name.Length;
+
+			Console.WriteLine("Введите вид питомца");
+			Pet.Type = Console.ReadLine();
+
+			Console.WriteLine("Введите возраст питомца");
+			Pet.Age = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("У вас есть собака по кличке персик и ему 13 лет верно?");
+			String yes = Console.ReadLine();
+
+		}
+	}
+}
